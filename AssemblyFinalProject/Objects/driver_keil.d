3522af9d092afb799964d0674d7283368de7e15c
Nour El-Deen M Ali <alinour64@yahoo.com>
2023-11-15 09:29:21 -0800

    test

---
+++ AssemblyFinalProject/Objects/driver_keil.d
@@ -0,0 +1,2 @@
+./objects/driver_keil.o: \
+  Assembly_Final_Project_Files\C_Programs\driver_keil.c
---
+++ AssemblyFinalProject/Objects/heap_template.d
@@ -0,0 +1,3 @@
+./objects/heap_template.o: \
+  Assembly_Final_Project_Files\C_Programs\heap_template.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h
